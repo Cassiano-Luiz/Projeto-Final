f0f9988abb71c16540853da147139b635e52f93c
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-11-10 15:06:51 -0300

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,29 +1,85 @@
-fun main(args: Array<String>) {
+import model.Course
+import utils.PrinterHelper.showMainMenu
+import utils.PrinterHelper.createStudentMenu
+import kotlin.Throws
+import kotlin.jvm.JvmStatic
+import service.StudentService
+import service.CourseService
+import java.text.ParseException
+import java.util.*
 
-    adicao(1, 2)
-    subtracao(3,4)
-    multiplicacao(5,6)
-    divisao(7.0,8.0)
-    potencia(9.0, 0.0)
-   }
+object Main {
+    @Throws(ParseException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val studentService = StudentService()
+        val cursoService = CourseService()
+        val scanner = Scanner(System.`in`)
+        var option = 0
+        do {
+            showMainMenu()
+            option = scanner.nextInt()
+            when (option) {
+                1 -> registerStudent(studentService, scanner)
+                2 -> findStudent(studentService, scanner)
+                3 -> enrollStudentToCourse(studentService, cursoService, scanner)
+                4 -> showStudentsSummary(studentService, scanner)
+                5 -> showCursosSummary(cursoService, scanner)
+            }
 
-   fun adicao( num1: Int, num2: Int){
-       var result = num1 + num2
-       print("o resultado da adição é: $result ")
-   }
-    fun subtracao ( num1: Int, num2: Int){
-        var result = num1 - num2
-        print("o resultado da subtração é: $result")
+        } while (option != 6)
     }
-    fun multiplicacao (num1: Int, num2: Int) {
-        var result = num1 * num2
-        println("o resultado da multiplicação é:$result")
+
+    private fun enrollStudentToCourse(
+        studentService: StudentService, courseService: CourseService,
+        scanner: Scanner
+    ) {
+        println("Insert student ID")
+        val studentId = scanner.next()
+        val student = studentService.findStudent(studentId)
+        if (student == null) {
+            println("Invalid Student ID")
+            return
         }
-    fun divisao(num1: Double, num2: Double){
-        var result = num1 / num2
-        println("o resultado da divisão é: $result")
+        println(student)
+        println("Insert course ID")
+        val courseId = scanner.next()
+        val course = courseService.getCourse(courseId)
+        if (course == null) {
+            println("Invalid Course ID")
+            return
+        }
+        println(course)
+        courseService.enrollStudent(courseId, student)
+        studentService.enrollToCourse(studentId, course)
+        println("Student with ID: $studentId enrolled successfully to $courseId")
     }
-    fun potencia(num1: Double, num2: Double){
-        var result = num1 / num2
-        println("o resultado da potencia é: $result")
+
+    private fun showCursosSummary(courseService: CourseService, scanner: Scanner) {
+        courseService.showSummary()
     }
+
+    private fun showStudentsSummary(studentService: StudentService, scanner: Scanner) {
+        studentService.showSummary()
+    }
+
+    private fun findStudent(studentService: StudentService, scanner: Scanner) {
+        println("Enter student ID: ")
+        val studentId = scanner.next()
+        val student = studentService.findStudent(studentId)
+        if (student != null) {
+            println("Student Found: ")
+            println(student)
+        } else {
+            println("Student with Id = $studentId not found")
+        }
+    }
+
+    @Throws(ParseException::class)
+    private fun registerStudent(studentService: StudentService, scanner: Scanner) {
+        val student = createStudentMenu(scanner)
+        studentService.subscribeStudent(student)
+    }
+
+
+}
